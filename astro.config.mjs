c354c38d153ae8f7ca01e074c82d3117c15ac4ef
Stefen Phelps <stefen@kelp.agency>
2022-10-14 15:25:08 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+// https://astro.build/config
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
 	site: "https://kelpagency.netlify.app/",
-	integrations: [prefetch()]
+	integrations: [prefetch(), sitemap()]
 });
