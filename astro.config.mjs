af9a625c9407e9dd95d0daa3bd142655d7b990bf
Stefen Phelps <stefen@kelp.agency>
2022-10-18 21:41:00 -0400

    improve image loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kelpagency.netlify.app/",
-  integrations: [prefetch(), sitemap(), compress()]
+	site: "https://kelpagency.netlify.app/",
+	integrations: [prefetch(), sitemap(), astroImageTools, compress()]
 });
