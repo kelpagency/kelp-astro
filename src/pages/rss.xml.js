7a50a8cdaa361876fca72afcce9c96ad97c23bd5
Stefen Phelps <stefen@kelp.agency>
2023-02-11 12:37:23 -0500

    add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+
+export async function get(context) {
+	const response = await fetch(`${import.meta.env.PUBLIC_WP_URL}/posts?per_page=100`);
+	const posts = await response.json();
+	console.log(posts);
+	return rss({
+		title: 'Kelp Current',
+		description:
+			"Here you'll find expert tips and tricks from the best of Kelp. Design tips, development tricks, and maybe even some tasty strategy treats",
+		site: context.site,
+		items: posts.map((post) => ({
+			title: post.title.rendered,
+			description: post.excerpt.rendered,
+			link: `/blog/${post.slug}`,
+			pubDate: post.date
+		})),
+		customData: `<language>en-us</language>`,
+		stylesheet: '/rss/styles.xsl'
+	});
+}
