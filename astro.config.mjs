e2119dee527e5a217c1d0ddeea0b9fa9a8a903e3
Stefen Phelps <stefen@kelp.agency>
2023-03-07 11:01:51 -0500

    restore

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import { astroImageTools } from 'astro-imagetools';
-import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.kelp.agency/',
-  integrations: [
-  // prefetch(),
-  sitemap(), astroImageTools, compress({
-    css: false,
-    html: true,
-    js: true,
-    img: false,
-    svg: false
-  }), critters()]
+	site: 'https://www.kelp.agency/',
+	integrations: [
+		// prefetch(),
+		sitemap(),
+		astroImageTools,
+		compress({
+			css: false,
+			html: true,
+			js: true,
+			img: false,
+			svg: false
+		})
+	]
 });
