097baa00e29c2a5738bef42972c30acf64549baf
Stefen Phelps <stefen@kelp.agency>
2023-03-07 10:22:51 -0500

    implement critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,18 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import { astroImageTools } from 'astro-imagetools';
+import critters from "astro-critters";
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://www.kelp.agency/',
-	integrations: [
-		// prefetch(),
-		sitemap(),
-		astroImageTools,
-		compress({
-			css: false,
-			html: true,
-			js: true,
-			img: false,
-			svg: true
-		})
-	]
+  site: 'https://www.kelp.agency/',
+  integrations: [
+  // prefetch(),
+  sitemap(), astroImageTools, compress({
+    css: false,
+    html: true,
+    js: true,
+    img: false,
+    svg: false
+  }), critters()]
 });
