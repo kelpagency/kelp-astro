9bc9970bf0583cb57958e72f6772444d32272bc5
Stefen Phelps <stefen@kelp.agency>
2023-06-04 19:11:43 -0400

    add related posts to blog sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,11 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
 import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.kelp.agency/',
-	integrations: [
-		sitemap(),
-		astroImageTools,
-		compress({
-			css: false,
-			html: true,
-			js: true,
-			img: false,
-			svg: false
-		})
-	]
+	compressHTML: true,
+	integrations: [sitemap(), astroImageTools]
 });
