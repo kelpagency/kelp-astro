5e7a0824909bf75bde070746919261d07fc8ffb9
Stefen Phelps <stefen@kelp.agency>
2023-08-07 20:03:19 -0400

    further reduce client-side js

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import { astroImageTools } from 'astro-imagetools';
 import critters from 'astro-critters';
-
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.kelp.agency/',
-	compressHTML: false,
-	integrations: [sitemap(), astroImageTools, critters(), prefetch()]
+  site: 'https://www.kelp.agency/',
+  compressHTML: false,
+  integrations: [sitemap(), astroImageTools, critters(), prefetch()],
 });
