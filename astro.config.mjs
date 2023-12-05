774af41e9e0898b62fdc7014b2d47c6c529c9843
Stefen Phelps <stefen@kelp.agency>
2023-12-05 13:36:45 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import { astroImageTools } from 'astro-imagetools';
-// import critters from 'astro-critters';
-import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.kelp.agency/',
 	compressHTML: false,
-	integrations: [sitemap(), astroImageTools, prefetch()]
+	integrations: [sitemap(), astroImageTools],
+	prefetch: true
 });
