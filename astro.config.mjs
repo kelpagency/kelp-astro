2c3eb1c9f06ec5593a882d219e0004196c98e09c
Stefen Phelps <stefen@kelp.agency>
2026-08-03 10:53:16 -0400

    fix CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,28 +11,6 @@ export default defineConfig({
   markdown: {
     syntaxHighlight: false,
   },
-  security: {
-    csp: {
-      directives: [
-        "default-src 'self'",
-        "connect-src 'self' https://admin.kelp.agency https://ingesteer.services-prod.nsvcs.net https://static.hsappstatic.net",
-        "font-src 'self' https:",
-        "frame-src 'self' https://store.kelp.agency https://static.hsappstatic.net https://www.youtube.com https://www.youtube-nocookie.com https://codepen.io https://videopress.com https://player.vimeo.com https://vimeo.com",
-        "img-src 'self' data: https:",
-        "media-src 'self' data: https:",
-        "object-src 'none'",
-        "base-uri 'self'",
-        "form-action 'self'",
-        "upgrade-insecure-requests",
-      ],
-      scriptDirective: {
-        resources: ["'self'", "data:", "https://static.hsappstatic.net"],
-      },
-      styleDirective: {
-        resources: ["'self'", "'unsafe-inline'"],
-      },
-    },
-  },
   image: {
     remotePatterns: [
       {
